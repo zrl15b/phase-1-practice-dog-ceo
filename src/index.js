48288a9bdecfbe521fb6547d8732eb16effc9d97
Zachary <zacharylampi96@gmail.com>
2022-06-13 15:56:04 -0400

    commit tree 1f572f53aad0e2c9c7c324ef016fa74151c7d800

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(`${imgUrl}`)
+.then(resp => resp.json())
+.then(x => {console.log(x.message)
+    x.message.forEach(createDog)})
+// .then(dog => dog.message.forEach(element => createDog(element)))
+
+function createDog(input) {     
+        let list=document.querySelector("#dog-breeds")
+        let li =document.createElement("li")
+        const img = document.createElement('img')
+        img.scr=input
+        console.log(input)
+        debugger
+        list.append(li,img)
+  }
